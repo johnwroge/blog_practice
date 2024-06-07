59c428967969a60632826a44024fc12ba6e34e84
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-06-07 16:49:55 -0400

    finished edit, create, delete

--- python_flask/server.py
+++ python_flask/server.py
@@ -1,7 +1,10 @@
 import sqlite3
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, request, url_for, flash, redirect,abort
+# form submissions 
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super secret key'
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -18,6 +21,29 @@ def get_post(post_id):
         abort(404)
     return post
 
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    # if the user clicked on Submit, it sends post request
+    if request.method == 'POST':
+        # Get the title and save it in a variable
+        title = request.form['title']
+        # Get the content the user wrote and save it in a variable
+        content = request.form['content']
+        if not title:
+            flash('Title is required!')
+        else:
+            # Open a connection to databse
+            conn = get_db_connection()
+            # Insert the new values in the db
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                        (title, content))
+            conn.commit()
+            conn.close()
+            # Redirect the user to index page
+            return redirect(url_for('index'))
+    return render_template('create.html')
+
+
 @app.route('/<int:post_id>')
 def post(post_id):
     # we got the post the user clicked on through the function we wrote before,
@@ -27,6 +53,38 @@ def post(post_id):
     #why? to be able to use it in the html page
     return render_template('post.html', post=post)
     
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    # Get the post to be edited by it's id
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            # Update the table 
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
 
 @app.route('/')
 def index():
